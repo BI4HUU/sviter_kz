14775953db6458cdb2f729b5d1028b0d3495712a
BI4HUU <BI4HUU@yandex.ru>
2019-12-23 11:41:18 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,10 +1,9 @@
 <?php
-
-    $to = "be4huu@gmail.com";
-    $subject = "Sviter";
+	$to = "nikita.volfer@gmail.com";
+	$subject = "Sviter";
     $message = implode (" | ", $_POST );
     $headers = "From: info@sviter.kz\r\nReply-to: info@sviter.kz\r\nContent-type: text/plain; charset=utf-8\r\n";
     mail($to, $subject, $message, $headers);
-
-?> 
+	file_get_contents('https://api.telegram.org/bot876993138:AAFtqXVt6q9nBte8DRn9t67qVs1OhB6M5VY/sendMessage?chat_id=389818935&text=' . $message)
+?>
 <?php  echo 'test' ?>
